44ebad81edd4e67ad020cdeacd1aac972e5f6850
Emilian <32312356+gliese436@users.noreply.github.com>
2020-05-08 23:24:15 +0200

    Add files via upload

---
+++ constants.php
@@ -0,0 +1,12 @@
+<?php
+/*** Global vars. ***/
+define("G_DEBUG", false);
+
+define("G_DB_HOST", "");
+define("G_DB_USER", "");
+define("G_DB_PASS", "");
+
+define("G_DB_NAME_MAIN", "");
+define("G_DB_DEFAULT_NAME", "");
+
+?>
